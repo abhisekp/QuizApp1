46349cb01607d55c0f3b60d405404f6950670136
Abhisek Pattnaik <abhisekp@engineer.com>
2014-05-13 20:24:41 +0530

    Added Description

--- src/com/abhisekp/Quiz.java
+++ src/com/abhisekp/Quiz.java
@@ -3,10 +3,10 @@ package com.abhisekp;
 /**
  * Author: AbhisekP
  * Creation Date: 13-05-2014 08:15 PM
- * Description:
+ * Description: Create a Quiz interface to build & play quiz
  */
 public class Quiz {
 	public static void main(String[] args) {
-
+		
 	}
 }
